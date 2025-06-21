0dfb3a7e049541ab71f24482095f50ecf2c65a10
João Paulo <jotap.abc@gmail.com>
2025-06-20 21:25:25 -0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1750465524024

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/[root-of-the-server]__1a9f3e9a._.js",
+  "static/chunks/[next]_internal_font_google_inter_59dee874_module_bfeb2d0c.css"
+],
+    source: "dynamic"
+});
